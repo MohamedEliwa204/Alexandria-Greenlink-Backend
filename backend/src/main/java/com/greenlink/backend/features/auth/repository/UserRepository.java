4c4c176208d8f3b3beb83a61a48784b9900dbcae
MohamedElshewy <abdalluhmohamed@gmail.com>
2025-11-30 17:53:05 +0200

    model and repo

--- backend/src/main/java/com/greenlink/backend/features/auth/repository/UserRepository.java
+++ backend/src/main/java/com/greenlink/backend/features/auth/repository/UserRepository.java
@@ -1,4 +1,10 @@
 package com.greenlink.backend.features.auth.repository;
 
-public interface UserRepository {
+import com.greenlink.backend.features.auth.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
 }
