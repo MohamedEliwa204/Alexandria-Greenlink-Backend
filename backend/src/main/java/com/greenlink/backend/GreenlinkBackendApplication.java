91a42d6cd8211c2d717cdedbf79e3e835a06231d
MohamedElshewy <abdalluhmohamed@gmail.com>
2025-11-30 07:45:41 +0200

    first commit

---
+++ backend/src/main/java/com/greenlink/backend/GreenlinkBackendApplication.java
@@ -0,0 +1,13 @@
+package com.greenlink.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenlinkBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreenlinkBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/greenlink/backend/GreenlinkBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greenlink.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreenlinkBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
