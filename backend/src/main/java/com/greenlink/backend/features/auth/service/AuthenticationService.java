aef880d1d85bd3ed9ec24cfb335339d708d4ce24
MohamedElshewy <abdalluhmohamed@gmail.com>
2025-11-30 20:32:20 +0200

    implement user registration and authentication in AuthenticationService; add ApplicationConfig for security configuration

--- backend/src/main/java/com/greenlink/backend/features/auth/service/AuthenticationService.java
+++ backend/src/main/java/com/greenlink/backend/features/auth/service/AuthenticationService.java
@@ -3,18 +3,60 @@ package com.greenlink.backend.features.auth.service;
 import com.greenlink.backend.features.auth.dto.AuthenticationRequest;
 import com.greenlink.backend.features.auth.dto.AuthenticationResponse;
 import com.greenlink.backend.features.auth.dto.RegisterRequest;
-import org.jspecify.annotations.Nullable;
+import com.greenlink.backend.features.auth.model.Role;
+import com.greenlink.backend.features.auth.model.User;
+import com.greenlink.backend.features.auth.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class AuthenticationService {
 
 
-    public @Nullable AuthenticationResponse register(RegisterRequest request) {
-        return null;
+    private final UserRepository repository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
+    private final AuthenticationManager authManager;
+
+
+    public AuthenticationResponse register(RegisterRequest request) {
+        var user = User.builder()
+                .firstname(request.firstname())
+                .lastname(request.lastname())
+                .email(request.email())
+                .password(passwordEncoder.encode(request.password()))
+                .role(Role.USER)
+                .build();
+
+        repository.save(user);
+
+        var jwtToken = jwtService.generateToken(user);
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .build();
+
     }
 
-    public @Nullable AuthenticationResponse authenticate(AuthenticationRequest request) {
-        return null;
+    public AuthenticationResponse authenticate(AuthenticationRequest request) {
+        authManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.email(),
+                        request.password()
+                )
+        );
+
+        var user = repository.findByEmail(request.email())
+                .orElseThrow();
+
+        var jwtToken = jwtService.generateToken(user);
+
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .build();
     }
 }
