c252993b65bf8d09dbe2b6aedad517da22b5e091
Saker55 <faressaker666@gmail.com>
2025-12-01 20:37:46 +0200

    database added

---
+++ database/demo/src/main/java/com/example/demo/entity/Ticket.java
@@ -0,0 +1,90 @@
+package com.example.demo.entity;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "Tickets")
+public class Ticket {
+
+    private Long Client_id;
+    private Long Price;
+    private String From ;
+    private String To ;
+    private LocalDate Date ;
+    private Long TransNumber;
+
+    public Ticket() {
+    }
+
+    public Ticket(Long Client_id, Long Price, String From, String To, LocalDate Date, Long TransNumber) {
+        this.Client_id = Client_id;
+        this.Price = Price;
+        this.From = From;
+        this.To = To;
+        this.Date = Date;
+        this.TransNumber = TransNumber;
+    }
+
+    public Long getClient_id() {
+        return Client_id;
+    }
+
+    public void setClient_id(Long Client_id) {
+        this.Client_id = Client_id;
+    }
+
+    public String getFrom() {
+        return From;
+    }
+
+    public void setFrom(String From) {
+        this.From = From;
+    }
+
+    public String getTo() {
+        return To;
+    }
+
+    public void setTo(String To ) {
+        this.To = To;
+    }
+
+    public LocalDate getDate() {
+        return Date;
+    }
+
+    public void setDate(LocalDate Date) {
+        this.Date = Date;
+    }
+
+    public Long getTransNumber() {
+        return TransNumber;
+    }
+
+    public void setTransNumber(Long TransNumber) {
+        this.TransNumber = TransNumber;
+    }
+
+    public Long  getPrice() {
+        return Price;
+    }
+
+    public void setPrice(Long Price) {
+        this.Price = Price;
+    }
+
+    @Override
+    public String toString() {
+        return "Ticket{"
+                + "Client_id=" + Client_id
+                + ", Price='" + Price + '\''
+                + ", TransNumber='" + TransNumber + '\''
+                + ", To='" + To + '\''
+                + ", Date='" + Date + '\''
+                + ", From='" + From + '\''
+                + '}';
+    }
+}
