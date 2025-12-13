60e781089d05a4ea38eb06711220fb66325c02c4
MohamedElshewy <abdalluhmohamed@gmail.com>
2025-12-13 23:57:09 +0200

    add CopilotDiffState configuration for tracking pending diffs

--- database/demo/src/main/java/com/example/demo/entity/ClientDTOmapper.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.entity;
-public class ClientDTOmapper {
-
-    public ClientDTO mapper(Client client){
-        return new ClientDTO(client.getFullName(), client.getEmail(), client.getTeleNumber());
-    }
-} 
